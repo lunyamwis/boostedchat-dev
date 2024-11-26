fc1ebd3c26c22d1207bf6a4669b8ce7b652545ce
dennis mokaya <anyoksdenn@gmail.com>
2024-11-26 11:47:58 +0300

    load env

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./vitest.setup.mjs",
-  },
-});
+
+
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./vitest.setup.mjs",
+    },
+  });
+}
