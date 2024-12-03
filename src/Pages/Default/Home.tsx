02fe0c55a417b4786e738ed714fa295a43a66975
dennis mokaya <anyoksdenn@gmail.com>
2024-12-03 07:01:35 +0300

    Make stats page main page

--- src/Pages/Default/Home.tsx
+++ src/Pages/Default/Home.tsx
@@ -5,7 +5,8 @@ import { pageData } from "..";
 export function Home() {
   const navigate = useNavigate();
   React.useEffect(() => {
-    navigate(pageData.Threads.url ?? "/threads");
+    // navigate(pageData.Threads.url ?? "/threads");
+    navigate(pageData.AccountsCanban.url ?? "/dashboard/accounts");
   }, [navigate]);
   return <>Home</>;
 }
